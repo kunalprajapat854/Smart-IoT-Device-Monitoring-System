d01c4ba83243fa83b482eb37770c32c7d7b8b664
kunalprajapat854 <kunalprajapat221@gmail.com>
2026-03-05 16:38:07 +0530

    creating alert service and injecting alert repository

--- src/main/java/in/iot/service/AlertService.java
+++ src/main/java/in/iot/service/AlertService.java
@@ -1,5 +1,50 @@
 package in.iot.service;
 
-public interface AlertService {
+import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.iot.Repository.AlertRepository;
+import in.iot.dto.TelemetryRequest;
+import in.iot.model.Alert;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+
+public class AlertService {
+
+	@Autowired
+    private  AlertRepository
+    alertRepository;
+
+    public void checkAlerts(TelemetryRequest req){
+
+        if(req.getTemperature()>80){
+
+            createAlert(req.getDeviceId(),
+                    "HIGH_TEMP",
+                    "Temperature exceeded limit");
+        }
+
+        if(req.getBatteryLevel()<20){
+
+            createAlert(req.getDeviceId(),
+                    "LOW_BATTERY",
+                    "Battery below threshold");
+        }
+    }
+
+    private void createAlert(String deviceId,String type,String msg){
+
+        Alert alert = new Alert();
+
+        alert.setDeviceId(deviceId);
+        alert.setType(type);
+        alert.setMessage(msg);
+        alert.setCreatedAt(LocalDateTime.now());
+
+		alertRepository.save(alert);
+    }
 }
