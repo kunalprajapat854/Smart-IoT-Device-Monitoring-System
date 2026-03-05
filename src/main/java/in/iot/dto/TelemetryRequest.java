6d4ee947673e61e3978a0b142fe3dbeb80b5b626
kunalprajapat854 <kunalprajapat221@gmail.com>
2026-03-05 16:30:08 +0530

    add setter-getter method

--- src/main/java/in/iot/dto/TelemetryRequest.java
+++ src/main/java/in/iot/dto/TelemetryRequest.java
@@ -1,5 +1,58 @@
 package in.iot.dto;
 
+import lombok.Data;
+
+@Data
 public class TelemetryRequest {
+
+	private String deviceId;
+
+	private Double temperature;
+
+	private Integer batteryLevel;
+
+	private Double latitude;
+
+	private Double longitude;
+
+	public String getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	public Double getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(Double temperature) {
+		this.temperature = temperature;
+	}
+
+	public Integer getBatteryLevel() {
+		return batteryLevel;
+	}
+
+	public void setBatteryLevel(Integer batteryLevel) {
+		this.batteryLevel = batteryLevel;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
 
 }
